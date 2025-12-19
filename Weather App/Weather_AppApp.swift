4bdde02304f108271557406592c207d11024a8ae
Vanshika Pal <vanshikapal451@gmail.com>
2025-12-19 12:38:23 +0530

    Initial Commit

---
+++ Weather App/Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_AppApp.swift
+//  Weather App
+//
+//  Created by Vanshika Pal on 19/12/25.
+//
+
+import SwiftUI
+
+@main
+struct Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
