820e08bf62cbec83bd42ab51647375b8036eaec3
Vanshika Pal <vanshikapal451@gmail.com>
2025-12-25 21:23:23 +0530

    Add binding and state

--- Weather App/ContentView.swift
+++ Weather App/ContentView.swift
@@ -8,17 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var isNight = false
+    
     var body: some View {
             ZStack {
-                BackgroundView()
-                    .edgesIgnoringSafeArea(.all)
+                
+                BackgroundView(isNight: $isNight)
+                
                 VStack {
-                    LocationView()
+                    LocationView(cityName: "Cupertino, CA")
                     
                     Spacer()
                     
-                    CurrentWeatherView()
-                        .padding(.bottom, 40)
+                    CurrentWeatherView(image: isNight ? "moon.stars.fill" :"cloud.sun.fill",
+                                       temp: isNight ? "18" : "76")
                     
                     Spacer()
                     
@@ -29,7 +33,13 @@ struct ContentView: View {
                     }
                     
                     Spacer()
-                    ButtonView()
+                    Button {
+                        isNight.toggle()
+                    } label: {
+                        ButtonView(title: "Change Day Time",
+                                   textColor: .blue,
+                                   backgroundColor: .white)
+                    }
 
                     
                 }
@@ -38,6 +48,60 @@ struct ContentView: View {
     }
 }
 
+struct BackgroundView: View {
+    @Binding var isNight: Bool
+    
+    var body: some View {
+        LinearGradient(gradient: Gradient(colors: [isNight ? .black : .blue,
+                                                   isNight ? .gray : Color(.skyBlue)]),
+                       startPoint: .topTrailing, endPoint: .bottom)
+        .edgesIgnoringSafeArea(.all)
+    }
+}
+
+
+struct ButtonView: View {
+    var title: String
+    var textColor: Color
+    var backgroundColor: Color
+    
+    var body: some View {
+            Text(title)
+                .font(.system(size: 25, weight: .medium))
+                .foregroundStyle(textColor)
+                .padding()
+                .background(backgroundColor)
+                .cornerRadius(10)
+    }
+}
+
+struct LocationView: View {
+    
+    var cityName: String
+    var body: some View {
+        Text(cityName)
+            .font(.largeTitle).bold()
+            .foregroundStyle(.white)
+            .padding()
+    }
+}
+
+struct CurrentWeatherView: View {
+    var image: String
+    var temp: String
+    
+    var body: some View {
+        Image(systemName: image)
+            .renderingMode(.original)
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .frame(width: 180, height: 180)
+        Text("\(temp)°")
+            .font(.system(size: 50, weight: .bold))
+            .fontWeight(.bold)
+            .foregroundStyle(.white)
+    }
+}
 #Preview {
     ContentView()
 }
